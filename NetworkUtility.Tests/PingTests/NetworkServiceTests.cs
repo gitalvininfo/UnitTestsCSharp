096c3952e3f10a3645a5aaf9fb46d14a6190655c
gitalvininfo <yanson.alvin.info@gmail.com>
2024-09-22 13:14:41 +0800

    string and int fluent assertions

--- NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using FluentAssertions;
+using NetworkUtility.Ping;
 
 namespace NetworkUtility.Tests.PingTests
 {
-    internal class NetworkServiceTests
+    public class NetworkServiceTests
     {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            //Arrange - variables, classes, mocks
+            var pingService = new NetworkService();
+
+            //Act
+            var result = pingService.SendPing();
+
+            //Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+
+        [Theory] // allow passing of variables
+        [InlineData(1, 2, 3)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+        {
+            //Arrange
+            var pingService = new NetworkService();
+
+            //Act
+            var result = pingService.PingTimeout(a, b);
+
+            //Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(3);
+            result.Should().NotBeInRange(-1000, 0);
+        }
     }
 }
